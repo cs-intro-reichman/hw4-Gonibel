6dd530d3266ebae542ab7147e2985e50fffd4245
Gonibel <gbelkine@gmail.com>
2024-12-01 16:54:52 +0200

    HW4 update 3

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -1,3 +1,5 @@
+//import com.apple.laf.resources.aqua;
+
 public class KeywordsDetector {
     public static void main(String[] args) {
         String[] sentences = {
@@ -22,5 +24,70 @@ public class KeywordsDetector {
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         // Replace this comment with your code
+        String str ="";
+        for( int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].toLowerCase();
+            str += sentences[i];
+        }
+        for(int i = 0; i < keywords.length; i++) {
+            if(contains(str, keywords[i])) {
+                System.out.println(keywords[i]);
+            }
+        }
+
+    }
+
+    public static String lowerCase(String str) {
+        // Replace the following statement with your code
+        String newstr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) > 64 && str.charAt(i) < 91) {
+                newstr += (char) (str.charAt(i) + 32);
+            }
+           // if (str.charAt(i) > 96 && str.charAt(i) < 123 || str.charAt(i) == 32) {
+           //     newstr += (char) str.charAt(i);
+             else {
+                newstr += str.charAt(i);
+            }
+
+        }
+        return newstr;
+    }
+
+    /** If str1 contains str2, returns true; otherwise returns false. */
+    public static boolean contains(String str1, String str2) {
+        // Replace the following statement with your code
+        if (str2.compareTo("") == 0) {return true;}
+        while (firstchar(str1, str2.charAt(0)) != -1) {
+            int i = firstchar(str1, str2.charAt(0));
+            String s = miniString(str1, i, i + str2.length());
+            if(s.compareTo(str2) == 0) {return true;}
+            else {
+                str1 = removeOne(str1, str1.indexOf(str2.charAt(0)));
+            }
+            if (str1.length() < str2.length()) {return false;}
+            if (str1.compareTo(str2) != 0 &&  str1.length() == str2.length()) {return false;}
+        }
+        return false;
+    }
+
+    public static int firstchar(String str, char c) {
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c) {return i;}
+        }
+        return -1;
+    }
+
+    public static String miniString(String str, int x, int y) {
+        String s = str.substring(x, y);
+        return s;
+
+    }
+    public static String removeOne(String str, int x) {
+        str = str.substring(0, x) + str.substring(x + 1);
+        return str;
+
     }
 }
+
+
