ba993afe4f7338bc4d0daf3c0538ac500edd37a6
Gonibel <gbelkine@gmail.com>
2024-12-01 13:38:36 +0200

    HW4 update 2

--- Primes.java
+++ Primes.java
@@ -1,9 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
-    }
-
-    public static void primes(int n) {
+        int n = Integer.parseInt(args[0]);
         boolean[] arr = new boolean[n + 1];
         double counter = 0;
         for(int i = 0; i < arr.length; i++) {
